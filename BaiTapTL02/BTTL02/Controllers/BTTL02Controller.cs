8ecc748ab836c4867a849367525783436f5a303e
VuTam1605 <vtam85133@gmail.com>
2024-08-27 15:25:16 +0700

    UploadBTTL02
    Update

---
+++ BaiTapTL02/BTTL02/Controllers/BTTL02Controller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BTTL02.Controllers
+{
+    public class BTTL02Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
