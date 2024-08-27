4e86eece123a154d5a4954750cba106a9e707d69
VuTam1605 <vtam85133@gmail.com>
2024-08-27 15:32:18 +0700

    Delete

--- BT03.cs/BT03.cs/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BT03.cs.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
