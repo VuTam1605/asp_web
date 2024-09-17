d42fdc36e238548e70464d85ee21820184ed7234
VuTam1605 <vtam85133@gmail.com>
2024-09-17 10:09:16 +0700

    Update

---
+++ BT07A/BT07A/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BT07A.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
